8f2105b47af4ce4cbdd2b16aa675cee4886c8988
agencare89 <agencare@uwo.ca>
2016-09-19 13:23:08 -0400

    relaunch for heroku

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 //config/database.js
 
 module.exports = { 
-	'url': 'mongodb://agencare:Hockey10!@ds051970.mongolab.com:51970/passport'
+	'url': 'mongodb://agencare:Hockey10!@ds053380.mongolab.com:53380/stocksapp'
 }
